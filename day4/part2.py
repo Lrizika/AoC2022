50709e14b76b92a91593128cf8ab94bdc34387ca
larkeith <llarkeith@gmail.com>
2022-12-08 20:14:23 -0800

    day 4 part 2

---
+++ day4/part2.py
@@ -0,0 +1,32 @@
+
+import sys
+
+DAY = 4
+TEST = 'test' in sys.argv
+path = f'day{DAY}/test' if TEST else f'day{DAY}/input'
+with open(path, 'r') as f:
+	lines = f.readlines()
+
+
+def get_section_overlap(s1: range, s2: range) -> range:
+	left_bound = max(min(s1), min(s2))
+	right_bound = min(max(s1), max(s2))
+	return range(left_bound, right_bound + 1)
+
+
+def get_range(range_str: str) -> range:
+	rmin, rmax = range_str.split("-")
+	# print(f'{range_str}: {range(int(rmin), int(rmax) + 1)}')
+	return range(int(rmin), int(rmax) + 1)
+
+
+overlap_count = 0
+for line in lines:
+	s1_str, s2_str = line.rstrip('\n').split(',')
+	s1, s2 = get_range(s1_str), get_range(s2_str)
+	overlap = get_section_overlap(s1, s2)
+	if len(overlap):
+		overlap_count += 1
+
+print(f'Overlap count: {overlap_count}')
+
