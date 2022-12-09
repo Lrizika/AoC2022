636ccad747e98e633878196ff57a9693960cb3fc
larkeith <llarkeith@gmail.com>
2022-12-08 21:30:23 -0800

    day 6 part 1

--- day6/part1.py
+++ day6/part1.py
@@ -7,44 +7,15 @@ VERBOSE = 'v' in sys.argv or '-v' in sys.argv
 directory = path.dirname(path.abspath(__file__))
 path = path.join(directory, 'test') if TEST else path.join(directory, 'input')
 with open(path, 'r') as f:
-	lines = f.readlines()
+	line = f.readline()
 
 
-def parse_stacks(lines):
-	for box_footer_index, line in enumerate(lines):
-		if line[:2] == ' 1':
-			box_count = int(line.rstrip(' \n').split(' ')[-1])
+for index, c in enumerate(line):
+	if index >= 4:
+		if len(set(line[index - 4: index])) == 4:
+			if VERBOSE:
+				print(set(line[index - 4: index]))
 			break
-	stacks = {v: [] for v in range(1, box_count + 1)}
-	for line in reversed(lines[:box_footer_index]):
-		for box in range(1, box_count + 1):
-			box_index = box * 4 - 3
-			if box_index < len(line) and line[box_index] != ' ':
-				stacks[box].append(line[box_index])
-	return stacks, box_footer_index, box_count
 
-
-def do_single_move(stacks, move):
-	origin = int(move.lstrip(' from').split(' ')[0])
-	destination = int(move.split(' ')[-1])
-	stacks[destination].append(stacks[origin].pop())
-
-
-def do_move(stacks, move):
-	count = int(move.lstrip('move ').split(' ')[0])
-	for _ in range(count):
-		do_single_move(stacks, move.lstrip(' move1234567890'))
-
-
-stacks, box_footer_index, box_count = parse_stacks(lines)
-for line in lines[box_footer_index + 1:]:
-	if line == '\n':
-		continue
-	do_move(stacks, line.rstrip('\n'))
-
-top_of_stacks = [stacks[box][-1] for box in range(1, box_count + 1)]
-
-print(f'Uppermost boxen: {"".join(top_of_stacks)}')
-
-
+print(f'End of marker index: {index}')
 
